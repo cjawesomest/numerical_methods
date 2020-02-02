879cf5d1f6f67165cd6da50f800162561de7554f
cjawesomest <cjcalv@gmail.com>
2020-02-02 17:59:49 -0500

    Finished matrix inverse, the way I already did it...

--- Week 4/matinv.py
+++ Week 4/matinv.py
@@ -1,11 +1,12 @@
 ## Cameron Calv ECE T480
 # Performs the inverse of a square matrix A. The input is a square matrix A, and the
 # output is a square matrix.
+from inv_lu import inv_lu
 
 def matinv(sqr_matrix):
-    inverse = float('nan')
+    inverse = inv_lu(sqr_matrix)
     return inverse
 
 if __name__ == '__main__':
-    matrix = [[]]
+    matrix = [[3, -0.1, -0.2], [0.1, 7, -0.3], [0.3, -0.2, 10]]
     print(matinv(matrix))
